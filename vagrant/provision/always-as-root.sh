fd9de39919b9627246e0c24d5acca62015b71d97
Thiago Senna <trsenna@gmail.com>
2018-05-23 23:51:31 -0300

    code cleanup

--- vagrant/provision/always-as-root.sh
+++ vagrant/provision/always-as-root.sh
@@ -2,17 +2,4 @@
 
 #== Bash helpers ==
 
-function info {
-  echo " "
-  echo "--> $1"
-  echo " "
-}
-
 #== Provision script ==
-
-info "Provision-script user: `whoami`"
-
-info "Restart web-stack"
-service php7.0-fpm restart
-service nginx restart
-service mysql restart
