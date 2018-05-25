5d42c6b668b279bb08727e9824fcb7776f140cfc
Thiago Senna <trsenna@gmail.com>
2018-05-24 23:59:53 -0300

    better provisioning

--- vagrant/provision/provision-databases.sh
+++ vagrant/provision/provision-databases.sh
@@ -3,6 +3,16 @@
 echo "mysql-server mysql-server/root_password password root@secret" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root@secret" | debconf-set-selections
 
+# Is MySQL installed?
+if [[ -f "/usr/bin/mysql" ]]; then
+  is_mysql_installed=true
+fi
+
+# Is PostgreSQL installed?
+if [[ -f "/usr/bin/psql" ]]; then
+    is_postgresql_installed=true
+fi
+
 # Install packages
 apt-get install -y \
   mysql-server \
@@ -10,21 +20,29 @@ apt-get install -y \
   sqlite3 libsqlite3-dev
 
 # Create MySQL privileges
-mysql --user="root" --password="root@secret" -e "CREATE USER 'gozma18'@'localhost' IDENTIFIED BY 'gozma18@secret';"
-mysql --user="root" --password="root@secret" -e "GRANT ALL ON *.* TO 'gozma18'@'localhost' IDENTIFIED BY 'gozma18@secret' WITH GRANT OPTION;"
-mysql --user="root" --password="root@secret" -e "GRANT ALL ON *.* TO 'gozma18'@'%' IDENTIFIED BY 'gozma18@secret' WITH GRANT OPTION;"
-mysql --user="root" --password="root@secret" -e "FLUSH PRIVILEGES;"
+if [[ ! -v is_mysql_installed ]]; then
+  mysql --user="root" --password="root@secret" -e "CREATE USER 'gozma18'@'localhost' IDENTIFIED BY 'gozma18@secret';"
+  mysql --user="root" --password="root@secret" -e "GRANT ALL ON *.* TO 'gozma18'@'localhost' IDENTIFIED BY 'gozma18@secret' WITH GRANT OPTION;"
+  mysql --user="root" --password="root@secret" -e "GRANT ALL ON *.* TO 'gozma18'@'%' IDENTIFIED BY 'gozma18@secret' WITH GRANT OPTION;"
+  mysql --user="root" --password="root@secret" -e "FLUSH PRIVILEGES;"
+fi
 
 # Create MySQL databases
-mysql --user="root" --password="root@secret" -e "CREATE DATABASE gozma18 character set UTF8mb4 collate utf8mb4_bin;"
-mysql --user="root" --password="root@secret" -e "CREATE DATABASE gozma18_test character set UTF8mb4 collate utf8mb4_bin;"
+if [[ ! -v is_mysql_installed ]]; then
+  mysql --user="root" --password="root@secret" -e "CREATE DATABASE gozma18 character set UTF8mb4 collate utf8mb4_bin;"
+  mysql --user="root" --password="root@secret" -e "CREATE DATABASE gozma18_test character set UTF8mb4 collate utf8mb4_bin;"
+fi
 
 # Create PostgreSQL privileges
-sudo -u postgres psql -c "CREATE ROLE gozma18 LOGIN PASSWORD 'gozma18@secret' SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
+if [[ ! -v is_postgresql_installed ]]; then
+  sudo -u postgres psql -c "CREATE ROLE gozma18 LOGIN PASSWORD 'gozma18@secret' SUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;"
+fi
 
 # Create PostgreSQL databases
-sudo -u postgres /usr/bin/createdb --echo --owner=gozma18 gozma18
-sudo -u postgres /usr/bin/createdb --echo --owner=gozma18 gozma18_test
+if [[ ! -v is_mysql_installed ]]; then
+  sudo -u postgres /usr/bin/createdb --echo --owner=gozma18 gozma18
+  sudo -u postgres /usr/bin/createdb --echo --owner=gozma18 gozma18_test
+fi
 
 # Restart services
 service mysql restart
--- vagrant/provision/provision-webserver.sh
+++ vagrant/provision/provision-webserver.sh
@@ -4,12 +4,11 @@
 apt-get install -y \
   apache2 php7.2 \
   libapache2-mod-php7.2 \
-  php7.2-cli php7.2-dev \
+  php7.2-cli php7.2-common php7.2-dev \
   php7.2-pgsql php7.2-sqlite3 php7.2-gd \
   php7.2-curl php7.2-memcached \
   php7.2-imap php7.2-mysql php7.2-mbstring \
   php7.2-xml php7.2-zip php7.2-bcmath php7.2-soap \
-  php7.2-intl php7.2-readline \
-  php7.2-common php7.2-opcache \
+  php7.2-intl php7.2-readline php7.2-opcache \
   php7.2-xmlrpc php7.2-imagick \
   php-xdebug php-pear
