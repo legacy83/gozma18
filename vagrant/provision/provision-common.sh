b84671d4257816a23801c4aaa30f59aa77805ec5
Thiago Senna <trsenna@gmail.com>
2018-05-31 16:19:32 -0300

    saving refactoring in common

--- vagrant/provision/provision-common.sh
+++ vagrant/provision/provision-common.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
-#== Variables ==
 #== Functionality ==
+
+common_update() {
+  apt-get update
+  apt-get upgrade -y
+}
+
+common_install() {
+  apt-get install -y \
+    colordiff dos2unix gettext \
+    graphviz imagemagick \
+    git-core subversion \
+    ngrep wget unzip zip \
+    whois vim mcrypt \
+    bash-completion zsh
+}
+
 #== Provisioning Script ==
 
 export DEBIAN_FRONTEND=noninteractive
-
-# Update packages
-apt-get update
-apt-get upgrade -y
-
-# Install packages
-apt-get install -y \
-  colordiff dos2unix gettext \
-  graphviz imagemagick \
-  git-core subversion \
-  ngrep wget unzip zip \
-  whois vim mcrypt \
-  bash-completion zsh
+common_update
+common_install
